2e6dbcb7f33ce0bb345ef8b3e0b26d5576f5bf19
Nathan Swartz <swartzn@netapp.com>
2019-08-07 09:27:59 -0500

    Add target host check to santricity_lun_mapping lookup plugin.

--- plugins/lookup/santricity_lun_mapping.py
+++ plugins/lookup/santricity_lun_mapping.py
@@ -22,39 +22,42 @@ class LookupModule(LookupBase):
         mapping_info = list()
         for volume in volumes:
 
-            # Existing lun mappings
-            if volume["host"] in luns_by_target:
-                for mapped_volume_name, mapped_lun in luns_by_target[volume["host"]]:
-                    if mapped_volume_name == volume["name"]:
-                        mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": mapped_lun})
-                        break
-                else:
+            # Check whether storage pool has specified a target host
+            if "host" in volume:
+
+                # Existing lun mappings
+                if volume["host"] in luns_by_target:
+                    for mapped_volume_name, mapped_lun in luns_by_target[volume["host"]]:
+                        if mapped_volume_name == volume["name"]:
+                            mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": mapped_lun})
+                            break
+                    else:
+                        used_list = [lun for vol, lun in luns_by_target[volume["host"]]]
+                        next_lun = 1
+                        while next_lun in used_list:
+                            next_lun += 1
+
+                        luns_by_target[volume["host"]].append(('_', next_lun))
+                        mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": next_lun})
+
+                # Map volume to host groups
+                elif volume["host"] in inventory["groups"]:
+                    mapped_luns = []
+                    for host in inventory["groups"][volume["host"]]:
+                        if host in luns_by_target:
+                            mapped_luns.extend(luns_by_target[host])
+                    luns_by_target.update({volume["host"]: mapped_luns})
+
                     used_list = [lun for vol, lun in luns_by_target[volume["host"]]]
                     next_lun = 1
                     while next_lun in used_list:
                         next_lun += 1
-
-                    luns_by_target[volume["host"]].append(('_', next_lun))
                     mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": next_lun})
 
-            # Map volume to host groups
-            elif volume["host"] in inventory["groups"]:
-                mapped_luns = []
-                for host in inventory["groups"][volume["host"]]:
-                    if host in luns_by_target:
-                        mapped_luns.extend(luns_by_target[host])
-                luns_by_target.update({volume["host"]: mapped_luns})
-
-                used_list = [lun for vol, lun in luns_by_target[volume["host"]]]
-                next_lun = 1
-                while next_lun in used_list:
-                    next_lun += 1
-                mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": next_lun})
-
-            # Map volume to host
-            else:
-                luns_by_target.update({volume["host"]: [("access", 7), ('_', 1)]})
-                mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": 1})
+                # Map volume to host
+                else:
+                    luns_by_target.update({volume["host"]: [("access", 7), ('_', 1)]})
+                    mapping_info.append({"volume": volume["name"], "target": volume["host"], "lun": 1})
 
         # Add an manually created hosts
         return mapping_info
