97b51e4a3eaed0cb0a19b53a06ec0f8930c5b5e7
Nathan Swartz <swartzn@netapp.com>
2019-09-05 14:36:39 -0500

    Updated nac_santricity_auditlog to use e-series base class.

--- plugins/modules/nac_santricity_auditlog.py
+++ plugins/modules/nac_santricity_auditlog.py
@@ -54,7 +54,7 @@ options:
         default: no
 notes:
     - Check mode is supported.
-    - This module is currently only supported with the Embedded Web Services API v3.0 and higher.
+    - Use M(ssid=="0") to configure SANtricity Web Services Proxy auditlog settings otherwise.
 """
 
 EXAMPLES = """
@@ -79,73 +79,45 @@ msg:
     type: str
     sample: The settings have been updated.
 """
-
 import json
 
-from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.netapp_eseries.santricity.plugins.module_utils.santricity import request, eseries_host_argument_spec
+from ansible_collections.netapp_eseries.santricity.plugins.module_utils.santricity import NetAppESeriesModule
 from ansible.module_utils._text import to_native
 
-try:
-    from urlparse import urlparse, urlunparse
-except Exception:
-    from urllib.parse import urlparse, urlunparse
 
-
-class AuditLog(object):
+class NetAppESeriesAuditLog(NetAppESeriesModule):
     """Audit-log module configuration class."""
     MAX_RECORDS = 50000
-    HEADERS = {"Content-Type": "application/json",
-               "Accept": "application/json"}
 
     def __init__(self):
-        argument_spec = eseries_host_argument_spec()
-        argument_spec.update(dict(
-            max_records=dict(type="int", default=50000),
-            log_level=dict(type="str", default="writeOnly", choices=["all", "writeOnly"]),
-            full_policy=dict(type="str", default="overWrite", choices=["overWrite", "preventSystemAccess"]),
-            threshold=dict(type="int", default=90),
-            force=dict(type="bool", default=False)))
+        ansible_options = dict(max_records=dict(type="int", default=50000),
+                               log_level=dict(type="str", default="writeOnly", choices=["all", "writeOnly"]),
+                               full_policy=dict(type="str", default="overWrite", choices=["overWrite", "preventSystemAccess"]),
+                               threshold=dict(type="int", default=90),
+                               force=dict(type="bool", default=False))
+        super(NetAppESeriesAuditLog, self).__init__(ansible_options=ansible_options,
+                                                    web_services_version="02.00.0000.0000",
+                                                    supports_check_mode=True)
 
-        self.module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
         args = self.module.params
-
+        self.log_level = args["log_level"]
+        self.force = args["force"]
+        self.full_policy = args["full_policy"]
         self.max_records = args["max_records"]
-        if self.max_records < 100 or self.max_records > self.MAX_RECORDS:
-            self.module.fail_json(msg="Audit-log max_records count must be between 100 and 50000: [%s]"
-                                      % self.max_records)
         self.threshold = args["threshold"]
+
+        if self.max_records < 100 or self.max_records > self.MAX_RECORDS:
+            self.module.fail_json(msg="Audit-log max_records count must be between 100 and 50000: [%s]" % self.max_records)
+
         if self.threshold < 60 or self.threshold > 90:
             self.module.fail_json(msg="Audit-log percent threshold must be between 60 and 90: [%s]" % self.threshold)
-        self.log_level = args["log_level"]
-        self.full_policy = args["full_policy"]
-        self.force = args["force"]
-        self.ssid = args['ssid']
-        self.url = args['api_url']
-        if not self.url.endswith('/'):
-            self.url += '/'
-        self.creds = dict(url_password=args['api_password'],
-                          validate_certs=args['validate_certs'],
-                          url_username=args['api_username'], )
 
-        self.proxy_used = self.is_proxy()
-        self.check_mode = self.module.check_mode
+        self.proxy_used = not self.is_embedded()
 
-    def is_proxy(self):
-        """Determine whether the API is embedded or proxy."""
-        try:
-
-            # replace http url path with devmgr/utils/about
-            about_url = list(urlparse(self.url))
-            about_url[2] = "devmgr/utils/about"
-            about_url = urlunparse(about_url)
-
-            rc, data = request(about_url, timeout=300, headers=self.HEADERS, **self.creds)
-
-            return data["runningAsProxy"]
-        except Exception as err:
-            self.module.fail_json(msg="Failed to retrieve the webservices about information! Array Id [%s]. Error [%s]."
-                                      % (self.ssid, to_native(err)))
+        # Append web services proxy forward end point.
+        self.url_path_prefix = ""
+        if not self.is_embedded() and self.ssid != 0:
+            self.url_path_prefix = "storage-systems/%s/forward/devmgr/v2/" % self.ssid
 
     def get_configuration(self):
         """Retrieve the existing audit-log configurations.
@@ -153,16 +125,13 @@ class AuditLog(object):
         :returns: dictionary containing current audit-log configuration
         """
         try:
-            if self.proxy_used:
-                rc, data = request(self.url + "audit-log/config", timeout=300, headers=self.HEADERS, **self.creds)
+            if self.proxy_used and self.ssid == 0:
+                rc, data = self.request("audit-log/config")
             else:
-                rc, data = request(self.url + "storage-systems/%s/audit-log/config" % self.ssid,
-                                   timeout=300, headers=self.HEADERS, **self.creds)
+                rc, data = self.request(self.url_path_prefix + "storage-systems/1/audit-log/config")
             return data
         except Exception as err:
-            self.module.fail_json(msg="Failed to retrieve the audit-log configuration! "
-                                      "Array Id [%s]. Error [%s]."
-                                      % (self.ssid, to_native(err)))
+            self.module.fail_json(msg="Failed to retrieve the audit-log configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
 
     def build_configuration(self):
         """Build audit-log expected configuration.
@@ -187,51 +156,46 @@ class AuditLog(object):
     def delete_log_messages(self):
         """Delete all audit-log messages."""
         try:
-            if self.proxy_used:
-                rc, result = request(self.url + "audit-log?clearAll=True", timeout=300,
-                                     method="DELETE", headers=self.HEADERS, **self.creds)
+            if self.proxy_used and self.ssid == 0:
+                rc, result = self.request("audit-log?clearAll=True", method="DELETE")
             else:
-                rc, result = request(self.url + "storage-systems/%s/audit-log?clearAll=True" % self.ssid, timeout=300,
-                                     method="DELETE", headers=self.HEADERS, **self.creds)
+                rc, result = self.request(self.url_path_prefix + "storage-systems/1/audit-log?clearAll=True", method="DELETE")
         except Exception as err:
-            self.module.fail_json(msg="Failed to delete audit-log messages! Array Id [%s]. Error [%s]."
-                                  % (self.ssid, to_native(err)))
+            self.module.fail_json(msg="Failed to delete audit-log messages! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
 
     def update_configuration(self, update=None, body=None, attempt_recovery=True):
         """Update audit-log configuration."""
         if update is None or body is None:
             update, body = self.build_configuration()
 
-        if update and not self.check_mode:
+        if update and not self.module.check_mode:
             try:
-                if self.proxy_used:
-                    rc, result = request(self.url + "storage-systems/audit-log/config", timeout=300,
-                                         data=json.dumps(body), method='POST', headers=self.HEADERS,
-                                         ignore_errors=True, **self.creds)
+                if self.proxy_used and self.ssid == 0:
+                    rc, result = self.request("storage-systems/audit-log/config", data=json.dumps(body), method='POST', ignore_errors=True)
                 else:
-                    rc, result = request(self.url + "storage-systems/%s/audit-log/config" % self.ssid, timeout=300,
-                                         data=json.dumps(body), method='POST', headers=self.HEADERS,
-                                         ignore_errors=True, **self.creds)
+                    rc, result = self.request(self.url_path_prefix + "storage-systems/1/audit-log/config",
+                                              data=json.dumps(body), method='POST', ignore_errors=True)
 
                 if rc == 422:
                     if self.force and attempt_recovery:
                         self.delete_log_messages()
                         update = self.update_configuration(update, body, False)
                     else:
-                        self.module.fail_json(msg="Failed to update audit-log configuration! Array Id [%s]. Error [%s]."
-                                              % (self.ssid, to_native(rc, result)))
+                        self.module.fail_json(msg="Failed to update audit-log configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(rc, result)))
 
             except Exception as error:
-                self.module.fail_json(msg="Failed to update audit-log configuration! Array Id [%s]. Error [%s]."
-                                      % (self.ssid, to_native(error)))
+                self.module.fail_json(msg="Failed to update audit-log configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(error)))
         return update
 
     def update(self):
         """Update the audit-log configuration."""
         update = self.update_configuration()
-        self.module.exit_json(msg="Audit-log update complete", changed=update)
+        if update:
+            self.module.exit_json(msg="Audit-log update complete", changed=update)
+        else:
+            self.module.exit_json(msg="No audit-log changes required", changed=update)
 
 
 if __name__ == "__main__":
-    auditlog = AuditLog()
+    auditlog = NetAppESeriesAuditLog()
     auditlog.update()
