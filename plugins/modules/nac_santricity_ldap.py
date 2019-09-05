89bb26a2429ba825bebea099b2d9790d439d20c0
Nathan Swartz <swartzn@netapp.com>
2019-09-05 14:42:36 -0500

    Updated nac_santricity_ldap to use e-series base class.

--- plugins/modules/nac_santricity_ldap.py
+++ plugins/modules/nac_santricity_ldap.py
@@ -152,43 +152,37 @@ msg:
     type: str
     sample: The ldap settings have been updated.
 """
-
-import json
+from ansible_collections.netapp_eseries.santricity.plugins.module_utils.santricity import NetAppESeriesModule
+from ansible.module_utils._text import to_native
 
 try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
 
-from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.netapp_eseries.santricity.plugins.module_utils.santricity import request, eseries_host_argument_spec
-from ansible.module_utils._text import to_native
 
-
-class Ldap(object):
+class NetAppESeriesLdap(NetAppESeriesModule):
     NO_CHANGE_MSG = "No changes were necessary."
 
     def __init__(self):
-        argument_spec = eseries_host_argument_spec()
-        argument_spec.update(dict(
-            state=dict(type='str', required=False, default='present',
-                       choices=['present', 'absent']),
-            identifier=dict(type='str', required=False, ),
-            username=dict(type='str', required=False, aliases=['bind_username']),
-            password=dict(type='str', required=False, aliases=['bind_password'], no_log=True),
-            name=dict(type='list', required=False, ),
-            server=dict(type='str', required=False, aliases=['server_url']),
-            search_base=dict(type='str', required=False, ),
-            role_mappings=dict(type='dict', required=False, ),
-            user_attribute=dict(type='str', required=False, default='sAMAccountName'),
-            attributes=dict(type='list', default=['memberOf'], required=False)
-        ))
+        ansible_options = dict(state=dict(type='str', required=False, default='present', choices=['present', 'absent']),
+                               identifier=dict(type='str', required=False, ),
+                               username=dict(type='str', required=False, aliases=['bind_username']),
+                               password=dict(type='str', required=False, aliases=['bind_password'], no_log=True),
+                               name=dict(type='list', required=False, ),
+                               server=dict(type='str', required=False, aliases=['server_url']),
+                               search_base=dict(type='str', required=False, ),
+                               role_mappings=dict(type='dict', required=False, ),
+                               user_attribute=dict(type='str', required=False, default='sAMAccountName'),
+                               attributes=dict(type='list', default=['memberOf'], required=False))
 
-        required_if = [
-            ["state", "present", ["username", "password", "server", "search_base", "role_mappings", ]]
-        ]
+        required_if = [["state", "present", ["username", "password", "server", "search_base", "role_mappings", ]]]
 
-        self.module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True, required_if=required_if)
+        super(NetAppESeriesLdap, self).__init__(ansible_options=ansible_options,
+                                                web_services_version="02.00.0000.0000",
+                                                required_if=required_if,
+                                                supports_check_mode=True)
+
         args = self.module.params
         self.ldap = args['state'] == 'present'
         self.identifier = args['identifier']
@@ -200,18 +194,7 @@ class Ldap(object):
         self.role_mappings = args['role_mappings']
         self.user_attribute = args['user_attribute']
         self.attributes = args['attributes']
-
-        self.ssid = args['ssid']
-        self.url = args['api_url']
-        self.creds = dict(url_password=args['api_password'],
-                          validate_certs=args['validate_certs'],
-                          url_username=args['api_username'],
-                          timeout=60)
-
         self.check_mode = self.module.check_mode
-
-        if not self.url.endswith('/'):
-            self.url += '/'
 
         self.embedded = None
         self.base_path = None
@@ -246,32 +229,16 @@ class Ldap(object):
 
         return domain
 
-    def is_embedded(self):
-        """Determine whether or not we're using the embedded or proxy implemention of Web Services"""
-        if self.embedded is None:
-            url = self.url
-            try:
-                parts = urlparse.urlparse(url)
-                parts = parts._replace(path='/devmgr/utils/')
-                url = urlparse.urlunparse(parts)
-
-                (rc, result) = request(url + 'about', **self.creds)
-                self.embedded = not result['runningAsProxy']
-            except Exception as err:
-                self.module.fail_json(msg="Failed to determine the Web Services implementation type! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
-
-        return self.embedded
-
     def get_full_configuration(self):
         try:
-            rc, result = request(self.url + self.base_path, **self.creds)
+            rc, result = self.request(self.base_path)
             return result
         except Exception as err:
             self.module.fail_json(msg="Failed to retrieve LDAP configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
 
     def get_configuration(self, identifier):
         try:
-            rc, result = request(self.url + self.base_path + '%s' % identifier, ignore_errors=True, **self.creds)
+            rc, result = self.request(self.base_path + '%s' % identifier, ignore_errors=True)
             if rc == 200:
                 return result
             elif rc == 404:
@@ -298,7 +265,7 @@ class Ldap(object):
                 else:
                     api = self.base_path + '%s' % domain['id']
 
-                rc, result = request(self.url + api, method='POST', data=json.dumps(domain), **self.creds)
+                rc, result = self.request(api, method='POST', data=domain)
             except Exception as err:
                 self.module.fail_json(msg="Failed to modify LDAP configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
 
@@ -316,7 +283,7 @@ class Ldap(object):
             msg = "The LDAP domain configuration for [%s] was cleared." % identifier
             if not self.check_mode:
                 try:
-                    rc, result = request(self.url + self.base_path + '%s' % identifier, method='DELETE', **self.creds)
+                    rc, result = self.request(self.base_path + '%s' % identifier, method='DELETE')
                 except Exception as err:
                     self.module.fail_json(msg="Failed to remove LDAP configuration! Array Id [%s]. Error [%s]." % (self.ssid, to_native(err)))
         return msg, updated
@@ -330,7 +297,7 @@ class Ldap(object):
             msg = "The LDAP configuration for all domains was cleared."
             if not self.check_mode:
                 try:
-                    rc, result = request(self.url + self.base_path, method='DELETE', ignore_errors=True, **self.creds)
+                    rc, result = self.request(self.base_path, method='DELETE', ignore_errors=True)
 
                     # Older versions of NetApp E-Series restAPI does not possess an API to remove all existing configs
                     if rc == 405:
@@ -342,11 +309,12 @@ class Ldap(object):
         return msg, updated
 
     def get_base_path(self):
-        embedded = self.is_embedded()
-        if embedded:
+        if self.is_embedded():
             return 'storage-systems/%s/ldap/' % self.ssid
+        elif self.ssid != "0":
+            return 'storage-systems/%s/forward/devmgr/v2/storage-systems/1/ldap/' % self.ssid
         else:
-            return '/ldap/'
+            return 'ldap/'
 
     def update(self):
         self.base_path = self.get_base_path()
@@ -361,5 +329,5 @@ class Ldap(object):
 
 
 if __name__ == '__main__':
-    settings = Ldap()
+    settings = NetAppESeriesLdap()
     settings.update()
